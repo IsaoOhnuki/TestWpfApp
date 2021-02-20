00b1f7bda71bbe941b5257930916f6b1eedd4251
IsaoOhnuki <isa0_net@hotmail.com>
2021-02-20 15:58:05 +0900

    commit

--- ObjectAreaLibrary/AStar.cs
+++ ObjectAreaLibrary/AStar.cs
@@ -1,50 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ObjectAreaLibrary
 {
-    class AStar
+    using Heuristic = Func<System.Drawing.Point, int>;
+    using NodePoint = System.Drawing.Point;
+    using ObstacleRect = System.Drawing.Rectangle;
+    using Viewpoint = Func<System.Drawing.Point, System.Drawing.Point, IEnumerable<System.Drawing.Rectangle>, IEnumerable<System.Drawing.Point>>;
+
+    public class AStarNode
     {
-        public struct Node
+        public NodePoint NodePoint;
+        public int Forward;
+        public int Backward;
+        public int Cost;
+        public bool Inspected;
+        public bool Adopt;
+        public int Index;
+
+        private static int _creatIndex;
+        public static AStarNode Creator(bool resetndex = false)
         {
-            public int X;
-            public int Y;
-            public int Forward;
-            public int Backward;
-            public int Cost;
-            public bool Adopt;
-            public int Index;
-            private static int _creatIndex;
-            public static Node Creator(bool resetndex = false)
+            if (resetndex)
             {
-                if (resetndex)
-                {
-                    _creatIndex = 0;
-                }
-                return new Node()
-                {
-                    Index = ++_creatIndex,
-                };
+                _creatIndex = 0;
             }
+            return new AStarNode()
+            {
+                Index = ++_creatIndex,
+            };
         }
+    }
 
-        public SortedDictionary<(int X, int Y), Node> NodeCollection { get; } = new SortedDictionary<(int, int), Node>();
+    public class AStar
+    {
+        private SortedDictionary<NodePoint, AStarNode> NodeCollection { get; } = new SortedDictionary<NodePoint, AStarNode>();
 
-        public void AddNode(Node node)
+        private void AddNode(AStarNode node)
         {
-            NodeCollection.Add((node.X, node.Y), node);
+            NodeCollection.Add(node.NodePoint, node);
         }
 
-        public void RemoveNode(Node node)
+        private AStarNode NodeAt(NodePoint point)
         {
-            NodeCollection.Remove((node.X, node.Y));
+            return NodeCollection.Where(_ => _.Key == point).Select(_ => _.Value).FirstOrDefault();
         }
 
-        public IEnumerable<Node> AdoptList(Node node)
+        public IEnumerable<AStarNode> AdoptList()
         {
             return NodeCollection.Where(_ => _.Value.Adopt).Select(_ => _.Value).OrderBy(_ => _.Index);
+        }
+
+        public bool Exec(NodePoint startPos, NodePoint endPos, NodePoint maxPos, IEnumerable<ObstacleRect> obstacles, Viewpoint viewpoint, Heuristic heuristic)
+        {
+            var nodes = new List<AStarNode>();
+            var viewpoints = viewpoint(startPos, maxPos, obstacles);
+            foreach (var pos in viewpoints)
+            {
+                if (pos == endPos)
+                {
+                    return true;
+                }
+                var node = NodeAt(pos);
+                if (node == null)
+                {
+                    node = AStarNode.Creator();
+                    node.NodePoint = pos;
+                    node.Forward = heuristic(startPos);
+                    node.Backward = heuristic(endPos);
+                    node.Cost = node.Forward + node.Backward;
+                    AddNode(node);
+                }
+                nodes.Add(node);
+            }
+
+            bool result = false;
+            foreach (var node in nodes.Where(_ => !_.Inspected).OrderBy(_ => _.Cost).ThenBy(_ => _.Forward))
+            {
+                node.Inspected = true;
+                if (Exec(node.NodePoint, endPos, maxPos, obstacles, viewpoint, heuristic))
+                {
+                    node.Adopt = true;
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public int Heuristic(NodePoint point)
+        {
+            return (int)Math.Round(Math.Sqrt(point.X * point.X + point.Y * point.Y));
+        }
+
+        public IEnumerable<NodePoint> Viewpoint(NodePoint point, NodePoint maxPoint, IEnumerable<ObstacleRect> rects)
+        {
+            List<NodePoint> ret = new List<NodePoint>();
+            if (point.X - 1 >= 0)
+            {
+                var pos = new NodePoint(point.X - 1, point.Y);
+                if (rects.All(_ => !_.Contains(pos)))
+                {
+                    ret.Add(pos);
+                }
+            }
+            if (point.Y - 1 >= 0)
+            {
+                var pos = new NodePoint(point.X, point.Y - 1);
+                if (rects.All(_ => !_.Contains(pos)))
+                {
+                    ret.Add(pos);
+                }
+            }
+            if (point.X + 1 < maxPoint.X)
+            {
+                var pos = new NodePoint(point.X + 1, point.Y);
+                if (rects.All(_ => !_.Contains(pos)))
+                {
+                    ret.Add(pos);
+                }
+            }
+            if (point.Y + 1 < maxPoint.Y)
+            {
+                var pos = new NodePoint(point.X, point.Y + 1);
+                if (rects.All(_ => !_.Contains(pos)))
+                {
+                    ret.Add(pos);
+                }
+            }
+            return ret;
         }
     }
 }
---
+++ ObjectAreaLibrary/ShortPathLine.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObjectAreaLibrary
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ShortPathLine : UserControl
+    {
+        public ShortPathLine()
+        {
+            InitializeComponent();
+        }
+
+        public void SetLine(IEnumerable<AStarNode> nodes)
+        {
+
+        }
+
+        public static readonly DependencyProperty ShortPathProperty = DependencyProperty.Register(
+            nameof(ShortPath),
+            typeof(Geometry),
+            typeof(ShortPathLine),
+            new FrameworkPropertyMetadata(default(Geometry)));
+
+        public Geometry ShortPath
+        {
+            get { return (Geometry)GetValue(ShortPathProperty); }
+            set { SetValue(ShortPathProperty, value); }
+        }
+    }
+}
